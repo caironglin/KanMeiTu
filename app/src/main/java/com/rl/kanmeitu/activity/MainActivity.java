7cb80af06a0a58a0de4931631afc4fd582858f82
RLWater <1226054531@qq.com>
2019-07-04 22:42:03 +0800

    Add Logout status

--- app/src/main/java/com/rl/kanmeitu/activity/MainActivity.java
+++ app/src/main/java/com/rl/kanmeitu/activity/MainActivity.java
@@ -1,15 +1,28 @@
 package com.rl.kanmeitu.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.rl.kanmeitu.R;
+import com.rl.kanmeitu.util.SharedPrefrenceUtil;
 
 public class MainActivity extends AppCompatActivity {
+    SharedPrefrenceUtil sp = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sp = SharedPrefrenceUtil.getInstance(getApplicationContext());
+    }
+
+    public void onLogoutClick(View view) {
+        sp.setLogin(false);
+        Intent intent = new Intent(this,LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
