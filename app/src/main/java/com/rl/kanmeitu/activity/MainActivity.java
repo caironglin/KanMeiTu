98a795844349ad25f8229850164e975996953d99
RLWater <1226054531@qq.com>
2019-07-05 01:20:29 +0800

    Add auto gson network image list

--- app/src/main/java/com/rl/kanmeitu/activity/MainActivity.java
+++ app/src/main/java/com/rl/kanmeitu/activity/MainActivity.java
@@ -3,6 +3,10 @@ package com.rl.kanmeitu.activity;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,12 +14,16 @@ import android.view.View;
 
 import com.rl.kanmeitu.R;
 import com.rl.kanmeitu.adapter.ImageAdapter;
+import com.rl.kanmeitu.api.Api;
 import com.rl.kanmeitu.domain.Image;
+import com.rl.kanmeitu.domain.response.ListResponse;
 import com.rl.kanmeitu.util.SharedPrefrenceUtil;
 
 import java.util.ArrayList;
 
 public class MainActivity extends BaseActivity {
+
+    private ImageAdapter imageAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +39,38 @@ public class MainActivity extends BaseActivity {
         for (int i = 1; i < 10; i++) {
             datas.add(new Image(String.format("http://dev-courses-quick.oss-cn-beijing.aliyuncs.com/%d.jpg",i)));
         }
-        ImageAdapter imageAdapter = new ImageAdapter(this);
+//        fetchData(); // bug
+        imageAdapter = new ImageAdapter(this);
         rv.setAdapter(imageAdapter);
         imageAdapter.setData(datas);
+    }
+
+    private void fetchData() {
+        Api.getInstance()
+                .images()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<ListResponse<Image>>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(ListResponse<Image> imageListResponse) {
+                        imageAdapter.setData(imageListResponse.getData());
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        e.printStackTrace();
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
     }
 
     public void onLogoutClick(View view) {
--- app/src/main/java/com/rl/kanmeitu/util/Constants.java
+++ app/src/main/java/com/rl/kanmeitu/util/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
     public static final String PASSWORD = "123456789";
 
     // 模拟接口
-    public static final String ENDPOINT = "http://";
+    public static final String ENDPOINT = "http://rap2api.taobao.org/app/mock/162466/";
 
     public static final String ID = "ID";
 
